b8c7456537cbbd2e8f87802b0230868f2ffa22ee
Koma Human <58357980+Ivan-Corporation@users.noreply.github.com>
2023-05-17 15:00:27 +0300

    feat: new deploy - privacy police / meta fb / og fix / bug reports fix / external links for out team

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <meta
+          name="facebook-domain-verification"
+          content="lwsitoiny62r21xd2nyb8sf6s08et5"
+        />
       </Head>
       <Component {...pageProps} />
     </>
